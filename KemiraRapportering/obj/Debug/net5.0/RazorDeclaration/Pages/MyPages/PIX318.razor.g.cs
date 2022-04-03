b6ac594c2a0870415bf6be0f725d499b8ac8f4b1
isak.skeie <245362@usn.no>
2022-04-03 13:02:18 +0200

    razor component is fixed

--- KemiraRapportering/obj/Debug/net5.0/RazorDeclaration/Pages/MyPages/PIX318.razor.g.cs
+++ KemiraRapportering/obj/Debug/net5.0/RazorDeclaration/Pages/MyPages/PIX318.razor.g.cs
@@ -119,7 +119,7 @@ using BlazorDateRangePicker;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 506 "C:\Users\isak.skeie\source\repos\Kemira\KemiraRapportering\Pages\MyPages\PIX318.razor"
+#line 46 "C:\Users\isak.skeie\source\repos\Kemira\KemiraRapportering\Pages\MyPages\PIX318.razor"
        
 
 
@@ -172,7 +172,7 @@ using BlazorDateRangePicker;
 
     public filtering[] filter = new filtering[30];
     public bool FilterSet { get; set; }
-
+    private int k = 0;
 
     DateTimeOffset? StartDate { get; set; } = DateTime.Today.AddDays(-7);
     DateTimeOffset? EndDate { get; set; } = DateTime.Today.AddDays(0).AddTicks(-1);
@@ -241,9 +241,6 @@ using BlazorDateRangePicker;
         string sql = $"SELECT top ({ Queries.TableLen }) * FROM PIX318_ReseptData ORDER BY BatchNr DESC";
         recipes = await _db.GetRecipes(sql);
         RecipeRead.Table = recipes;
-
-
-
     }
 
     public void ResetDropDown()
